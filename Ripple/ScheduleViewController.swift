5cb389309462774eb710c3f8371c433c72c9d743
Adam <agluck2920@gmail.com>
2017-01-26 21:25:25 -0500

    push notifications update 1

--- Ripple/ScheduleViewController.swift
+++ Ripple/ScheduleViewController.swift
@@ -347,6 +347,25 @@ class ScheduleViewController: BaseViewController, JTCalendarDelegate, UITableVie
     let userManager: UserManager = {
         return UserManager()
     }()
+    func publishMessageAsPushNotificationSync(message: String, deviceId: String) -> MessageStatus? {
+        
+        
+        let deliveryOptions = DeliveryOptions()
+        deliveryOptions.pushSinglecast = [deviceId]
+        
+        let publishOptions = PublishOptions()
+        publishOptions.assignHeaders(["ios-text":"You have receieved a new message from"])
+        var error: Fault?
+        let messageStatus = Backendless.sharedInstance().messaging.publish("default", message: message,publishOptions:publishOptions,deliveryOptions:deliveryOptions,error: &error)
+        if error == nil {
+            print("MessageStatus = \(messageStatus.status) ['\(messageStatus.messageId)']")
+            return messageStatus
+        }
+        else {
+            print("Server reported an error: \(error)")
+            return nil
+        }
+    }
     
     //accepts the invitation and then removes it from the invitation list
     func acceptInvitationTouched(indexPath: NSIndexPath) {
@@ -355,6 +374,9 @@ class ScheduleViewController: BaseViewController, JTCalendarDelegate, UITableVie
             
             userManager.confirmFollowingRequest(withID: requestDetails.requestID, withCompletion: { [weak self] (success) in
                 if success {
+                    let user = UserManager().currentUser().name
+                    let deviceID = requestDetails.fromUser.getProperty("deviceID") as? String
+                    self!.publishMessageAsPushNotificationSync(user! + "has requested to follow you", deviceId: deviceID!)
                     self?.showAlert("Success".localized(), message: "You have a new follower!".localized())
                     self?.followingRequests.removeAtIndex(indexPath.row)
                     self?.tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Left)
